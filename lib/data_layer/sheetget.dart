48fa81a87f75f134da3961bdb0fafc1766de1abe
Kryštof Regent <1009@student.itgymnazium.cz>
2023-01-19 10:24:52 +0100

    DL-sessionLog---

--- lib/data_layer/sheetget.dart
+++ lib/data_layer/sheetget.dart
@@ -1,9 +1,18 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:flutter_session_manager/flutter_session_manager.dart';
+
 import 'package:sheetbrowse/2business_layer/approotdata.dart';
+
+var sessionManager = SessionManager();
+
+void sessionLog(String key, String value) {
+  sessionManager.set('__log__$key', value);
+}
 
 class GoogleSheets {
   final String? sheetId, sheetName;
@@ -18,10 +27,17 @@ class GoogleSheets {
     final url =
         'https://sheets.googleapis.com/v4/spreadsheets/$sheetId/values/$sheetName?key=$apiKey';
 
+    sessionLog('url-getAllSheet', url);
     try {
       final response = await http.get(Uri.parse(url));
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
       return extractedData['values'];
+    } on SocketException {
+      throw Failure(message: "Error: No Internet Connection. [getAllSheet]");
+    } on HttpException {
+      throw Failure(message: "Error: Internal Issue Occured. [getAllSheet]");
+    } on FormatException {
+      throw Failure(message: "Error: Bad Response. [getAllSheet]");
     } catch (e) {
       debugPrint(e.toString());
 
@@ -34,11 +50,17 @@ class GoogleSheets {
     String? apiKey = AppDataPrefs.getApikey();
     final url =
         'https://sheets.googleapis.com/v4/spreadsheets/$sheetId/values/$sheetName?key=$apiKey';
-
+    sessionLog('url-sheetValues', url);
     try {
       final response = await http.get(Uri.parse(url));
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
       return extractedData['values'];
+    } on SocketException {
+      throw Failure(message: "Error: No Internet Connection. [sheetValues]");
+    } on HttpException {
+      throw Failure(message: "Error: Internal Issue Occured. [sheetValues]");
+    } on FormatException {
+      throw Failure(message: "Error: Bad Response. [sheetValues]");
     } catch (e) {
       debugPrint(e.toString());
 
@@ -50,10 +72,16 @@ class GoogleSheets {
   Future selectData() async {
     String url =
         'https://script.google.com/macros/s/AKfycbzfN5YsBSbhwfk9FbbCmIFjz6wkBkHFXVCk6zMytHdjdUO6DjSL_OSKcgrVEWj81EpIww/exec?sheetName=starred2022&colLetter=B&value=@Dala';
-
+    sessionLog('url-sheetValues', url);
     try {
       final response = await http.get(Uri.parse(url));
       return jsonDecode(response.body);
+    } on SocketException {
+      throw Failure(message: "Error: No Internet Connection. [selectData]");
+    } on HttpException {
+      throw Failure(message: "Error: Internal Issue Occured. [selectData]");
+    } on FormatException {
+      throw Failure(message: "Error: Bad Response. [selectData]");
     } catch (e) {
       debugPrint(e.toString());
 
@@ -67,14 +95,31 @@ Future getTagQuote(String sourceSheetName, String id, String fileId) async {
   String? url = AppDataPrefs.getString('selectServiceUrl', '');
   url =
       '$url?action=getTagQuote&sourceSheetName=$sourceSheetName&ID=$id&fileId=$fileId';
-
+  sessionLog('url-getTagQuote', url);
   try {
     final response = await http.get(Uri.parse(url));
     return jsonDecode(response.body);
+  } on SocketException {
+    throw Failure(message: "Error: No Internet Connection. [getTagQuote]");
+  } on HttpException {
+    throw Failure(message: "Error: Internal Issue Occured. [getTagQuote]");
+  } on FormatException {
+    throw Failure(message: "Error: Bad Response. [getTagQuote]");
   } catch (e) {
     debugPrint(e.toString());
 
     return [];
     // you can show any error widgets for your users here.
+  }
+}
+
+class Failure {
+  final String message;
+  Failure({required this.message});
+
+  @override
+  String toString() {
+    sessionLog(message, 'Failure');
+    return message;
   }
 }
