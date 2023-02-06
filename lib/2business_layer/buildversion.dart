8a775bc018df843890379306b1e8ceb77b9dd74c
Kryštof Regent <1009@student.itgymnazium.cz>
2023-02-06 19:11:19 +0100

    A-value-of-type-canot-be-assigned-to-same---

--- lib/2business_layer/buildversion.dart
+++ lib/2business_layer/buildversion.dart
@@ -1,1 +1,1 @@
-const buildVersion = '--A-value-of-type--canot-be--assigned-to-same---06-02-2023 19:08:39,02';     
+const buildVersion = 'A-value-of-type-canot-be-assigned-to-same---06-02-2023 19:11:11,15';     
